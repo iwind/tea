c109a7abde80b58bf3a36228a30a85c837b02034
刘祥超 <q@yun4s.cn>
2017-05-03 10:25:06 +0800

    异常增加cause

--- tea/Exception.php
+++ tea/Exception.php
@@ -3,7 +3,15 @@
 namespace tea;
 
 class Exception extends \Exception {
+	private $_cause;
 
+	public function setCause($cause) {
+		$this->_cause = $cause;
+	}
+
+	public function cause() {
+		return $this->_cause;
+	}
 }
 
 ?>
